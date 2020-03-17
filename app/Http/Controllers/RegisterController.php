bcc898d9d9a82ad0512fb73cae9120f6d4582d8b
Thobeats <41216440+Thobeats@users.noreply.github.com>
2020-03-17 11:06:40 +0100

    first commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class RegisterController extends Controller
+{
+    public function store(Request $request){
+
+        $request->validate([
+            'name' => "required | unique:users,name",
+            'email' => "required | email | unique:users,email",
+            'pincode' => "required | digits:6 | unique:users,password"
+        ]);
+        $user = new User();
+
+        $user->name = request('name');
+        $user->email = request('email');
+        $user->password = request('pincode');
+
+        $user->save();
+        return redirect('/')->with('msg', "You have succesfully registered");
+    }
+
+    public function show(){
+        return view('welcome');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Register</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="container">
+        <div class='row'>
+            <div class='col-10'>
+                <h1 class='text-center'>Register Users</h1>
+            </div>        
+        </div>
+        
+        <div class="content">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li class="">{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        @if (session('msg'))
+            <div class="alert alert-success">
+                <p>{{ session('msg') }}</p>
+            </div>
+        @endif
+
+            <form action="/" method="POST">
+                @csrf
+                <div class="form-group">  
+                    <label for="name">Your name: </label>
+                    <input type="text" name="name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Your email: </label>
+                    <input type="text" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="pincode">Your pincode: </label>
+                    <input type="text" name="pincode">
+                </div>
+
+                <input type="submit" name="submit" value="Register">
+            </form>
+            
+            
+
+            
+        </div>
+    </div>
+    </body>
+</html>
