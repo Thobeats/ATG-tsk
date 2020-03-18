1a42d4f8a0ec4499bd6c4e01aca30d4e94ea032f
Thobeats <41216440+Thobeats@users.noreply.github.com>
2020-03-18 05:52:16 +0100

    new commit

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -10,8 +10,8 @@ class RegisterController extends Controller
     public function store(Request $request){
 
         $request->validate([
-            'name' => "required | unique:users,name",
-            'email' => "required | email",
+            'name' => "required",
+            'email' => "required",
             'pincode' => "required | digits:6 | unique:users,password"
         ]);
         $user = new User();
